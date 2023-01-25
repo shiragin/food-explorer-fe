be8a33ec2c884e98b0fdf491805fb0f3f8e64380
Shira Ginat <shiragin@gmail.com>
2023-01-25 17:58:10 +0200

    Fix card css

--- src/Components/RecipeCard/RecipeCard.js
+++ src/Components/RecipeCard/RecipeCard.js
@@ -1,46 +1,25 @@
-import axios from "axios";
-import { Button, Card, Col } from "react-bootstrap";
+import axios from 'axios';
+import { Button, Card, Col } from 'react-bootstrap';
+import '../../scss/RecipeCard.scss';
+import { useRecipesContext } from '../../Context/RecipesContext';
 
 export default function RecipeCard({ recipe }) {
-    console.log(recipe);
-
-//     <Container>
-//     <Row xs={1} md={3} lg={5}>
-//       {recipeList.map((rec) => {
-//         console.log(rec);
-//         return <RecipeCard recipe={rec} />;
-//       })}
-//     </Row>
-//   </Container>
-    
   return (
-    <Col>
-      <Card>
-        <Card.Header as="h5">
-            {recipe.strMeal}
-        </Card.Header>
-        <Card.Body>
-          
-          <Card.Text>
-            <img
-              src={recipe.strMealThumb}
-              alt="petPic"
-              className="imgPetCard"
-              width={"95%"}
-            />
-          </Card.Text>
-          <Card.Footer>
-            <Button
-              variant="primary"
-              onClick={() => {
-                console.log(recipe.idMeal);
-              }}
-            >
-              See More
-            </Button>{" "}
-          </Card.Footer>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Card>
+      <Card.Img variant='top' src={recipe.strMealThumb} alt='recipePic' />
+      <Card.Header as='h5'>{recipe.strMeal}</Card.Header>
+      <Card.Body>
+        <Card.Footer>
+          <Button
+            variant='primary'
+            onClick={() => {
+              console.log(recipe.idMeal);
+            }}
+          >
+            See More
+          </Button>{' '}
+        </Card.Footer>
+      </Card.Body>
+    </Card>
   );
 }
